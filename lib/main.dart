c18c847a55d4c23de0f6f2070d27ccfce09243dd
Haydee <a.21308051280493@cbtis128.edu.mx>
2024-04-26 18:07:09 +0000

    actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,128 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Inicio'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
   final String title;
-  const MyHomePage({super.key, required this.title});  
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedArt = "Ninguno seleccionado";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        body: Center(
+            child: Column(
+      children: [
+        const SizedBox(
+          height: 30,
         ),
-      ),
-    );
+        Text("Selecciona un articulo: $selectedArt"),
+        const SizedBox(height: 30),
+        SizedBox(
+          width: 250,
+          child: RadioMenuButton(
+            value: "Peluche",
+            groupValue: selectedArt,
+            onChanged: (selectedValue) {
+              setState(() => selectedArt = selectedValue!);
+            },
+            style: ButtonStyle(
+                shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )),
+                elevation: const MaterialStatePropertyAll(2),
+                backgroundColor: const MaterialStatePropertyAll(Colors.white)),
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                SizedBox(
+                  height: 60,
+                  child: Image.asset("images/peluche.jpg"),
+                ),
+                const SizedBox(width: 10),
+                const Text("Peluche"),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 30),
+        SizedBox(
+          width: 250,
+          child: RadioMenuButton(
+            value: "Mochila",
+            groupValue: selectedArt,
+            onChanged: (selectedValue) {
+              setState(() => selectedArt = selectedValue!);
+            },
+            style: ButtonStyle(
+                shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )),
+                elevation: const MaterialStatePropertyAll(2),
+                backgroundColor: const MaterialStatePropertyAll(Colors.white)),
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                SizedBox(
+                  height: 60,
+                  child: Image.asset("images/mochila.jpeg"),
+                ),
+                const SizedBox(width: 10),
+                const Text("Mochila"),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 30),
+        SizedBox(
+          width: 250,
+          child: RadioMenuButton(
+            value: "Pin",
+            groupValue: selectedArt,
+            onChanged: (selectedValue) {
+              setState(() => selectedArt = selectedValue!);
+            },
+            style: ButtonStyle(
+                shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )),
+                elevation: const MaterialStatePropertyAll(2),
+                backgroundColor: const MaterialStatePropertyAll(Colors.white)),
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                SizedBox(
+                  height: 60,
+                  child: Image.asset("images/pin.jpg"),
+                ),
+                const SizedBox(width: 10),
+                const Text("Pin"),
+              ],
+            ),
+          ),
+        ),
+      ],
+    )));
   }
 }
